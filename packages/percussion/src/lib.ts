76c2be37841980858ebf7a7cccb96c8a7fc6a186
Abe <abramabradley@gmail.com>
2021-11-21 19:26:05 -0500

    fixed some typos and lint errors

--- packages/percussion/src/lib.ts
+++ packages/percussion/src/lib.ts
@@ -65,7 +65,6 @@ export function getDrumBPM() {
     count = 0
     intCount = 0
   }
-
 }
 
 export function addDrum() {
@@ -137,6 +136,7 @@ interface SoundSettings {
   noise: AudioNodeConfig
   oscillation: AudioNodeConfig
 }
+
 // AB - can use like this -- export function playSound(setting: Settings) {
 export function playSound(setting) {
   const noiseFreq = setting[0]
@@ -194,45 +194,33 @@ export function playSound(setting) {
   oscillator.stop(audioContext.currentTime + oscillationTime)
 }
 
-
-
-
-
-
 // __________ Supporting Functions _____________
-
-
-
-
-
 
 /////////////////////////////////////////////////////////////////////////////////////////
 // _________ Functions for getting the tempo and quantization of hummed melodies__________
 
-
 //This is a helper function for the audioToMelody Function.
 //It takes an array of values that represent intervals between each note in the recording.
 //The interval values are expressed in multiples of the smallest interval in the recording.
-//So a 1 in the array would represent an interval time of 1 * the smallest interval 
+//So a 1 in the array would represent an interval time of 1 * the smallest interval
 //It returns the most common beat interval that the tempo will be based on
 function getCommonInterval(intervals) {
   const intervalCats = [] //this will hold the catagories of interval length
-  const intervalRanks = [] //this will hold the number of occurencies for each catagory 
+  const intervalRanks = [] //this will hold the number of occurrences for each category
 
   //record the first interval type and add 1 to its rank
   intervalCats.push(intervals.pop())
   intervalRanks.push(1)
 
-  //This while loop iterates until the intervals vector is exhausted 
+  //This while loop iterates until the intervals vector is exhausted
   while (intervals.length > 0) {
     let i = 0
 
-    //This for loop iterates over the intervalCats until it finds a match for the 
+    //This for loop iterates over the intervalCats until it finds a match for the
     //current interval
     for (; i < intervalCats.length; i++) {
-
       //if a match is found then pop that interval off the intervals vector
-      //and add one to its catagory rank. Then break to stop the search
+      //and add one to its category rank. Then break to stop the search
       if (intervalCats[i] == intervals[intervals.length - 1]) {
         intervalRanks[i]++
         intervals.pop()
@@ -241,7 +229,7 @@ function getCommonInterval(intervals) {
     }
 
     //if no match was found then pop off the interval from the intervals vector
-    //and add it to the intervalCats to make a new catagory of interval. set its 
+    //and add it to the intervalCats to make a new category of interval. set its
     //rank to 1
     if (i == intervalCats.length) {
       intervalCats.push(intervals.pop())
@@ -249,11 +237,10 @@ function getCommonInterval(intervals) {
     }
   }
 
-
   let maxRank = 0
   let commonInterval = null
 
-  //This for loop iterates over the interval ranks to find the most common occurence
+  //This for loop iterates over the interval ranks to find the most common occurrence
   for (let i = 0; i < intervalRanks.length; i++) {
     if (intervalRanks[i] > maxRank) {
       maxRank = intervalRanks[i]
@@ -262,75 +249,72 @@ function getCommonInterval(intervals) {
   }
 
   //return the most commonInterval as an integer that represent how many
-  //shortest intervals it is long. 
+  //shortest intervals it is long.
   return commonInterval
 }
 
-
 //These values are used to detect the areas where a note is being played.
-// - minFreq is the smallest frequency that you are interseted in detecting.
-//   It is used to avoid detecting the silences in between peaks in a notes frequeny
+// - minFreq is the smallest frequency that you are interested in detecting.
+//   It is used to avoid detecting the silences in between peaks in a notes frequency
 // - minAmp is the lowest volume at which a note will be detected.
 //Both of these values can be changed to make the code work better if needed, but
 //in my testing these values work quite well.
 const minFreq = 20
 const minAmp = 0.04
 
-//I use this to get the average amplitude over a region of the audio data. 
+//I use this to get the average amplitude over a region of the audio data.
 //This is important because it allows the us to avoid detecting the space in between
 //the peaks of a note's frequency
-const ave = arr => arr.reduce((a,b) => Math.abs(a) + Math.abs(b), 0) / arr.length
+const ave = arr =>
+  arr.reduce((a, b) => Math.abs(a) + Math.abs(b), 0) / arr.length
 
-
-//This function takes the a vector of audio data and calculates the 
+//This function takes the a vector of audio data and calculates the
 //quantization value and the tempo. At the moment this is all it does, but
 //it can be made to produce a vector that describes when a note should be played
 //and for how long.
 function audioToMelody(audioData: Float32Array) {
   const length = audioData.length
 
-  //minStep is the period of time inbetween the peaks of
-  //the minFreq. It is represented in the number of elements 
+  //minStep is the period of time in-between the peaks of
+  //the minFreq. It is represented in the number of elements
   //in the audio data that span that time
   const minStep = audioContext.sampleRate / minFreq
 
-  //quantLen will store the shortest interval in the audio data. 
+  //quantLen will store the shortest interval in the audio data.
   //In other words it is the duration of one beat according to whatever
   //the quantization value is
   let quantLen = length
 
-  let notes = []//stores the index at which notes start 
-  let note = false//tells the for loop if a note is currently playing
-  let step//holds the values within the range of minStep
+  const notes = [] //stores the index at which notes start
+  let note = false //tells the for loop if a note is currently playing
+  let step //holds the values within the range of minStep
 
   //This for loop iterates through the audioData minStep elements at a time
   //It records the index of the start of each note
-  for (let i = 0; i < length; i+=minStep) {
-    step = audioData.slice(i, i+minStep)//get the next minStep worth of elements
+  for (let i = 0; i < length; i += minStep) {
+    step = audioData.slice(i, i + minStep) //get the next minStep worth of elements
 
     //If a note was not already playing and the amplitude is higher than minAmp
     //Then record the start of a note and tell the note variable that a note is now playing
     if (!note && ave(step) > minAmp) {
       notes.push(i)
       note = true
-    } 
-    //else if a note is currently playing and the amplitude drops bellow minAmp 
+    }
+    //else if a note is currently playing and the amplitude drops bellow minAmp
     //Then tell the note variable that the note is no longer playing
     else if (note && ave(step) < minAmp) {
       note = false
     }
   }
 
+  const intervals = [] //This stores the intervals between the start of each note
+  let interval: number //holds the current interval length
 
-
-  let intervals = []//This stores the intervals between the start of each note
-  let interval //holds the current interval length
-
-  //This for loop iterates of the notes vector to find the shortest interval between 
-  //the start of two notes. The shortes interval, or quantLen, has to be found before recording all of 
+  //This for loop iterates of the notes vector to find the shortest interval between
+  //the start of two notes. The shortest interval, or quantLen, has to be found before recording all of
   //the intervals between notes because the intervals are recorded as multiples of the shortest interval
-  for (let i = 0; i < notes.length-1; i++) {
-    interval = notes[i+1] - notes[i]
+  for (let i = 0; i < notes.length - 1; i++) {
+    interval = notes[i + 1] - notes[i]
     if (interval < quantLen) {
       quantLen = interval
     }
@@ -338,44 +322,36 @@ function audioToMelody(audioData: Float32Array) {
 
   //This for loop iterates over the notes vector calculating and recording the interval
   //length between the start of each note
-  for (let i = 0; i < notes.length-1; i++) {
-
-    //calculate and record the current interval as an integer multiple of the 
+  for (let i = 0; i < notes.length - 1; i++) {
+    //calculate and record the current interval as an integer multiple of the
     //shortest interval. Recoding the intervals this way makes finding the most
-    //common interval significtly easier and more standardized
-    interval = notes[i+1] - notes[i]
+    //common interval significantly easier and more standardized
+    interval = notes[i + 1] - notes[i]
     intervals.push(parseInt((interval / quantLen).toFixed()))
   }
 
   //convert the quantLen from number of elements to time in seconds
-  quantLen /= audioContext.sampleRate 
+  quantLen /= audioContext.sampleRate
 
-  //quant is the quantization value. The getCommonInterval function returns the number of shortest 
-  //interval lengths that make up the most common interval. In other words it is the beats for 
+  //quant is the quantization value. The getCommonInterval function returns the number of shortest
+  //interval lengths that make up the most common interval. In other words it is the beats for
   //each whole note. It may be a good idea to pass 2 * quant as the quantization value, that way
-  //a note that is hummed twice quickly can be registered as two distict sounds rather than one 
-  //continuous one. 
-  let quant = getCommonInterval(intervals)
+  //a note that is hummed twice quickly can be registered as two distinct sounds rather than one
+  //continuous one.
+  const quant = getCommonInterval(intervals)
 
-  //tempo is expressed in beats per minute. quantLen * quant is the amount of 
+  //tempo is expressed in beats per minute. quantLen * quant is the amount of
   //time for a whole note in seconds (seconds/beat). Dividing 60 seconds by this value
   //gets beats/minute
-  let tempo = 60 / (quantLen * quant)
+  const tempo = 60 / (quantLen * quant)
 
   console.log(quant)
   console.log(tempo)
 }
 
-
 //////////////////////////////////////////////////////////////////////
 //____________End of functions for getting quantization and tempo for melodies//
 //////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
 
 function audioToDrum(audioData: Float32Array) {
   const length = audioData.length
@@ -415,7 +391,8 @@ function audioToDrum(audioData: Float32Array) {
   // minInterval is used to set the amount of time between playing each
   // tile on the drum machine.
   // AB - Math.min(..intervals) ??? -- gets the smallest value in an array
-  const interval = Math.min.apply(null, intervals) / (audioContext.sampleRate / 1000)
+  const interval =
+    Math.min.apply(null, intervals) / (audioContext.sampleRate / 1000)
   if (interval < minInterval && numDrums > 0) {
     resetDrums(interval)
     minInterval = interval
@@ -426,7 +403,12 @@ function audioToDrum(audioData: Float32Array) {
   const newDrum = []
   for (let b = 0; b < beats.length - 1; b++) {
     newDrum.push(1)
-    const space = parseInt((intervals[b] / (minInterval * (audioContext.sampleRate / 1000))).toFixed())
+    const space = parseInt(
+      (
+        intervals[b] /
+        (minInterval * (audioContext.sampleRate / 1000))
+      ).toFixed(),
+    )
     for (let i = 0; i < space - 1; i++) {
       newDrum.push(0)
     }
@@ -495,8 +477,27 @@ primaryGainControl.connect(audioContext.destination)
 
 // __Snare__
 
+const snareSoundObject: SoundSettings = {
+  noise: {
+    frequency: 2500,
+    gain: 0.1,
+    time: 0.1,
+    ramp: 0.1,
+    volume: 1,
+  },
+  oscillation: {
+    frequency: 250,
+    gain: 0.1,
+    time: 0.1,
+    ramp: 0.01,
+    volume: 1,
+  },
+}
+
+const snareSoundArray = [2500, 0.1, 0.1, 0.1, 1, 250, 0.1, 0.1, 0.01, 1]
+
 export function playSnare() {
-  playSound([2500, 0.1, 0.1, 0.1, 1, 250, 0.1, 0.1, 0.01, 1])
+  playSound(snareSoundArray)
 }
 
 // ___Kick __
