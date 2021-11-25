f6e44f330af32afebae757ef1ffdc18bc8b997a4
Ethan Rickert <ethanrickert0429@gmail.com>
2021-11-25 17:39:31 -0600

    added TODO

--- packages/pitch-finder/src/index.ts
+++ packages/pitch-finder/src/index.ts
@@ -1,10 +1,20 @@
 import { convertBufferToNotes } from './lib'
 
 void (async () => {
-  const response = await fetch('C-major.wav')
+  // TODO:
+  // Fetch the file provided by user and 
+  // plug that into "file" var,
+  // but also provide option to use sample file
+  // e.g. the default (C-major2.wav)
+
+  // example:
+  // defaultFile ? file = userFile : file = 'C-major2.wav'
+
+  let file = 'C-major2.wav'
+  const response = await fetch(file)
 
   if (!response.ok) {
-    throw Error('Response not okay')
+    throw Error('${file} not found!')
   }
   const buffer = await response.arrayBuffer()
   const notes = await convertBufferToNotes(buffer)
