688f785f9c3cf98532a18480877c23bc1a8e9124
Ethan <ethanrickert0429@gmail.com>
2021-11-29 22:10:38 -0500

    merge package implemented

--- packages/merge/src/main.ts
+++ packages/merge/src/main.ts
@@ -3,8 +3,10 @@ import Crunker from "crunker";
 const crunker = new Crunker();
 
 const mergedBlob = async () => {
+  // fetchAudio(file1ToMerge, File2ToMerge)
   const buffers = await crunker.fetchAudio("C-major.wav", "C-major2.wav");
   const mergedBuffers = await crunker.mergeAudio(buffers);
+  // export(mergedBuffers, fileFormat)
   const output = await crunker.export(mergedBuffers, "audio/wav");
   return output.blob;
 }
